ade54b4205f2675bca394039997b5815c10a2ca3
RodrigoSobral2000 <rodrigosobral@sapo.pt>
2020-12-19 17:45:54 +0000

    hard_test

--- Meta4/tests/random_stuff.c
+++ Meta4/tests/random_stuff.c
@@ -1,27 +1,43 @@
-void main(void) {
+int main(void) {
     int i=0;
     putchar(i);
     i=20;
     putchar(i);
 
-    short a='\n';
+    char a='A';
     while (a<'Z') {
         putchar(a);
         a=a+1;
     }
-/*
+    
     putchar('\n');
-    short i='\n';
+    short b='\n';
     while (i<'Z') {
         putchar(i);
         i=i+1;
     }
-    */
-    putchar(':');
-    short soma= getchar();
-    putchar(soma+1);
 
-    double resultado= 5*10;
+    putchar(':');
+    short f= getchar();
+    putchar(f+1);
+
+    double t= 5.0;
+    double resultado= 5*10.0;
+
+    if (t==42 && 4==3 || 4&3==5|2) { }
+    double soma = t+2;
+    double sub= t-1;
+    double mult= t*3;
+    double div= t/2;
+    int res1= (1*1)/1+1-1*(1+1);
+
+    putchar(res1);
+    if (!(1==1)) {
+        putchar(res1);
+    }
+
 
     putchar('\n');
+    return 0;
 }
+ 
